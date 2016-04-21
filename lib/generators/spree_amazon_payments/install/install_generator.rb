cca3e0c1af702d3f4d4a6ca932646eb28b743681
Manuel Martinez <vmmf08@gmail.com>
2016-04-21 18:44:03 -0500

    Remove empty assets, update the install generator

--- lib/generators/spree_amazon_payments/install/install_generator.rb
+++ lib/generators/spree_amazon_payments/install/install_generator.rb
@@ -1,36 +1,11 @@
-##
-# Amazon Payments - Login and Pay for Spree Commerce
-#
-# @category    Amazon
-# @package     Amazon_Payments
-# @copyright   Copyright (c) 2014 Amazon.com
-# @license     http://opensource.org/licenses/Apache-2.0  Apache License, Version 2.0
-#
-##
 module SpreeAmazonPayments
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
-      def create_javascripts
-        create_file 'vendor/assets/javascripts/spree/frontend/all.js' unless File.exists?('vendor/assets/javascripts/spree/frontend/all.js')
-        create_file 'vendor/assets/javascripts/spree/backend/all.js' unless File.exists?('vendor/assets/javascripts/spree/backend/all.js')
-      end
-
-      def create_stylesheets
-        create_file 'vendor/assets/stylesheets/spree/frontend/all.css' unless File.exists?('vendor/assets/stylesheets/spree/frontend/all.css')
-        create_file 'vendor/assets/stylesheets/spree/backend/all.css' unless File.exists?('vendor/assets/stylesheets/spree/backend/all.css')
-      end
-
-      def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_amazon_payments\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_amazon_payments\n"
-      end
-
       def add_stylesheets
         inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_amazon_payments\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_amazon_payments\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
