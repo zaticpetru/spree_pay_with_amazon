9bff3c83f2a0a18b985bab521fb336271508995c
Manuel Martinez <vmmf08@gmail.com>
2016-04-27 18:18:17 -0500

    Add comment to explain why we can't use_static_preferences

--- spec/models/spree_amazon/configuration_spec.rb
+++ spec/models/spree_amazon/configuration_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe SpreeAmazon::Configuration do
   describe '#use_static_preferences!' do
+    # We cannot use_static_preferences since we allow users
+    # to configure the settings through the admin UI, calling
+    # use_static_preferences will reset all settings to their
+    # default values and store them in memory
     it "raises an error" do
       configuration = SpreeAmazon::Configuration.new
 
