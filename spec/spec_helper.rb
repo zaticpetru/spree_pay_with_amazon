bdeb42395786b07ceb46692ae8281141b88e6778
Jordan Brough <git.j@brgh.net>
2016-08-24 12:33:50 -0600

    Fix WebMock to allow codeclimate
    As per codeclimate instructions:
    https://github.com/codeclimate/ruby-test-reporter/tree/v0.6.0#help-your-gem-is-raising-a-

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,8 @@ require 'database_cleaner'
 require 'ffaker'
 require 'webmock/rspec'
 
+WebMock.disable_net_connect!(allow: 'codeclimate.com')
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
