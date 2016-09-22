6f38e45722cda65762913042d4de3f734e35f0fe
Allison C. Reilly <acreilly3@gmail.com>
2016-09-22 13:28:43 -0400

    get_refund_details uses PayWithAmazon::Client

--- lib/amazon_mws.rb
+++ lib/amazon_mws.rb
@@ -127,11 +127,8 @@ class AmazonMws
     )
   end
 
-  def get_refund_details(ref_number)
-    process({
-      "Action" => "GetRefundDetails",
-      "AmazonRefundId" => ref_number
-      })
+  def get_refund_details(refund_id)
+    client.get_refund_details(refund_id)
   end
 
   def cancel
