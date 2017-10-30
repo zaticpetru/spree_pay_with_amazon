boomerdigital/solidus_amazon_payments

confirm_order uses PayWithAmazon::Client#confirm_order_reference

lib/amazon_mws.rb
@@ -63,10 +63,7 @@ class AmazonMws
   end
 
   def confirm_order
-    process({
-      "Action"=>"ConfirmOrderReference",
-      "AmazonOrderReferenceId" => @amazon_order_reference_id,
-    })
+    client.comfirm_order_reference(@amazon_order_reference_id)
   end
 
   def authorize(authorization_reference_id, total, currency, seller_authorization_note: nil)

zaticpetru/spree_pay_with_amazon

lib/amazon_mws.rb
@@ -81,10 +81,7 @@ class AmazonMws
   end
 
   def confirm_order
-    process({
-      "Action"=>"ConfirmOrderReference",
-      "AmazonOrderReferenceId" => @amazon_order_reference_id,
-    })
+    client.confirm_order_reference(@amazon_order_reference_id)
   end
 
   def authorize(authorization_reference_id, total, currency, seller_authorization_note: nil)