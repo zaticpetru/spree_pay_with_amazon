Lostmyname/solidus_amazon_payments

Use Amazon gem for SetOrderReferenceDetails request
Preparing to add more data in this call.

lib/amazon_mws.rb
@@ -60,6 +60,10 @@ class AmazonMws
     )
   end
 
+  def set_order_reference_details(total)
+    client.set_order_reference_details(@number, total, options)
+  end
+
   def set_order_data(total, currency)
     process({
       "Action"=>"SetOrderReferenceDetails",

zaticpetru/spree_pay_with_amazon

lib/amazon_mws.rb
@@ -60,6 +60,10 @@ class AmazonMws
     )
   end
 
+  def set_order_reference_details(total)
+    client.set_order_reference_details(@amazon_order_reference_id, total, options)
+  end
+
   def set_order_data(total, currency)
     process({
       "Action"=>"SetOrderReferenceDetails",