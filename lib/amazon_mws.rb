zaticpetru/spree_pay_with_amazon

commit 426b64da8877fd3846246fc8f41cd3524c9c1691

Make fetch_order_data use get_order_reference_details method

lib/amazon_mws.rb
@@ -56,7 +56,7 @@ class AmazonMws
       params.merge!('AddressConsentToken' => @address_consent_token)
     end
     AmazonMwsOrderResponse.new(
-      process(params)
+      get_order_reference_details(params)
     )
   end
 
@@ -79,8 +79,8 @@ class AmazonMws
     )
   end
 
-  def get_order_reference_details
-    client.get_order_reference_details(@amazon_order_reference_id)
+  def get_order_reference_details(params)
+    client.get_order_reference_details(params)
   end
 
   def confirm_order

commit d7e1c79353613171762e35284c92ceaa708aa878

Make `fetch_order_data` work
The original amazon implementation constructed the HTTP requests more
directly. Over the course of several commits this was refactored to use
the `pay_with_amazon` wrapper.

In 426b64da the `fetch_user_data` was refactored to proxy to the
`get_order_reference_details` method (which turn proxies to a method
of the same name in the pay_with_amazon` wrapper).

The problem is that the params were still based on the old HTTP
implementation rather than what `get_order_reference_details` needs.
In fact that method was also refactored to use a different method
signature than the method it proxed to.

Likely 426b64da broke both `fetch_user_data` and
`get_order_reference_details`.

This commit makes `get_order_reference_details` a more direct proxy. It
also makes `fetch_user_data` use the correct method signature.

lib/amazon_mws.rb
@@ -38,6 +38,7 @@ class AmazonMwsOrderResponse
 end
 
 class AmazonMws
+  delegate :get_order_reference_details, to: :client
 
   def initialize(amazon_order_reference_id, gateway:, address_consent_token: nil)
     @amazon_order_reference_id = amazon_order_reference_id
@@ -47,15 +48,8 @@ class AmazonMws
 
 
   def fetch_order_data
-    params = {
-      "Action"=>"GetOrderReferenceDetails",
-      "AmazonOrderReferenceId" => @amazon_order_reference_id,
-    }
-    if @address_consent_token
-      params.merge!('AddressConsentToken' => @address_consent_token)
-    end
     AmazonMwsOrderResponse.new(
-      get_order_reference_details(params)
+      get_order_reference_details(@amazon_order_reference_id, address_consent_token: @address_consent_token)
     )
   end
 
@@ -76,10 +70,6 @@ class AmazonMws
       total,
       currency_code: currency
     )
-  end
-
-  def get_order_reference_details(params)
-    client.get_order_reference_details(params)
   end
 
   def confirm_order